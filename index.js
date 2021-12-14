757e4ff24d795196f7754d74626524e137cf5a71
Chris Baum <Christopher.Baum@valpo.edu>
2021-12-13 21:00:14 -0500

    commit tree a7429ff881fd8711bbb2b3d08414ac7fb9dc5bc4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+
+const mainRemove = document.querySelector("main")
+mainRemove.remove()
+
+
+// create h1 element
+const newHeader = document.createElement('h1')
+
+// set h1 id to victory
+newHeader.setAttribute("id", "victory")
+
+// create text
+const newContent = document.createTextNode('Chris is the champion')
+
+//append header with text
+newHeader.appendChild(newContent)
+
+document.body.appendChild(newHeader)
