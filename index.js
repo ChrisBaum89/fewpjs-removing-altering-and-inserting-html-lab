755a872135cc07e90ff79afbd0fce50cdbffa67c
Chris Baum <Christopher.Baum@valpo.edu>
2021-12-13 21:00:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+const mainRemove = document.querySelector("main")
+mainRemove.remove()
+
+
+// create h1 element
+const newHeader = document.createElement('h1')
+
+// set h1 id to victory
+newHeader.setAttribute("id", "victory")
+
+// create text
+const newContent = document.createTextNode('Chris is the champion')
+
+//append header with text
+newHeader.appendChild(newContent)
+
+//modify the DOM
+document.body.appendChild(newHeader)
